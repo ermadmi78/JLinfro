78fa23a9a1e84ecc15b56dd10c1339448eef6a20
linfro <ermadmi78@gmail.com>
2014-05-31 13:05:52 +0400

    AbstractMultiWrapperValue

--- jlinfro-core/src/main/java/com/github/linfro/core/value/GetUnionValue.java
+++ jlinfro-core/src/main/java/com/github/linfro/core/value/GetUnionValue.java
@@ -1,49 +1,22 @@
 package com.github.linfro.core.value;
 
 import com.github.linfro.core.GetValue;
-import com.github.linfro.core.Getter;
-import com.github.linfro.core.ValueChangeListener;
 import com.github.linfro.core.dsl.GetValueHolder;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.github.linfro.core.common.ObjectUtil.notNull;
 
 /**
  * @author Dmitry Ermakov
  * @version 2014-02-16
  * @since 1.0.0
  */
-public class GetUnionValue<T> extends AbstractGetValue<List<T>> {
-    protected final List<GetValue<? extends T>> args;
-    protected final ValueChangeListener<T> argListener = new ValueChangeListener<T>() {
-        @Override
-        public void valueChanged(Getter<? extends T> getter) {
-            lastGetter = getter;
-            fireValueChanged();
-        }
-    };
-
+public class GetUnionValue<T> extends AbstractMultiWrapperValue<T, List<T>> {
     private List<T> result;
-    private Getter<? extends T> lastGetter;
 
     @SafeVarargs
     public GetUnionValue(GetValueHolder<? extends T> firstArg, GetValueHolder<? extends T>... otherArgs) {
-        GetValue<? extends T> firstValue = notNull(notNull(firstArg).getContentValue());
-
-        this.args = new ArrayList<>(otherArgs == null ? 1 : otherArgs.length + 1);
-        this.args.add(firstValue);
-        firstValue.addChangeListener(argListener);
-        this.lastGetter = firstValue;
-
-        if (otherArgs != null) {
-            for (GetValueHolder<? extends T> arg : otherArgs) {
-                GetValue<? extends T> nextValue = notNull(notNull(arg).getContentValue());
-                this.args.add(nextValue);
-                nextValue.addChangeListener(argListener);
-            }
-        }
+        super(firstArg, otherArgs);
     }
 
     @Override
@@ -65,54 +38,14 @@ public class GetUnionValue<T> extends AbstractGetValue<List<T>> {
     }
 
     @Override
-    public boolean isValueValid() {
-        if (args.isEmpty()) {
-            throw new IllegalStateException("Value is disposed");
-        }
-
-        for (GetValue<? extends T> arg : args) {
-            if (!arg.isValueValid()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    @Override
-    public Object getMetaInfo(String key) {
-        if (args.isEmpty()) {
-            throw new IllegalStateException("Value is disposed");
-        }
-
-        return lastGetter == null ? null : lastGetter.getMetaInfo(key);
-    }
-
-    @Override
     public void fireValueChanged() {
         result = null;
-        if (!args.isEmpty()) {
-            super.fireValueChanged();
-        }
-    }
-
-    @Override
-    public boolean canDispose() {
-        return listeners.isEmpty();
+        super.fireValueChanged();
     }
 
     @Override
     public void dispose() {
-        listeners.clear();
-
-        for (GetValue<? extends T> arg : args) {
-            arg.removeChangeListener(argListener);
-            if (arg.canDispose()) {
-                arg.dispose();
-            }
-        }
-        args.clear();
+        super.dispose();
         result = null;
-        lastGetter = null;
     }
 }
